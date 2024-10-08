a2cc7560ea27bbe9c9f0807e82a7bb25a83b543e
Marcin Bogusz <bogusz.marcin@icloud.com>
2024-10-08 08:28:41 +0200

    UI implemented.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from tkinter import *
 
-# ---------------------------- CONSTANTS ------------------------------- #
+# ---------------------------- CONSTANTS ----------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
@@ -9,10 +10,34 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+# ---------------------------- TIMER RESET ---------------------------- #
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------ #
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM -------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro App")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW,
+                    font=(FONT_NAME, 45, "bold"))
+check_label = Label(text="✓", fg=GREEN, bg=YELLOW,
+                    font=(FONT_NAME, 35, "bold"))
+start_button = Button(text="Start", highlightthickness=0)
+reset_button = Button(text="Reset", highlightthickness=0)
+
+canvas = Canvas(width=202, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white",
+                   font=(FONT_NAME, 35, "bold"))
+
+timer_label.grid(row=0, column=1)
+canvas.grid(row=1, column=1)
+start_button.grid(row=3, column=0)
+check_label.grid(row=4, column=1)
+reset_button.grid(row=3, column=2)
+
+window.mainloop()
