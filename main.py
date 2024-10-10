ea03a92bcc7daffc28ef2c327d34b4950d4bd842
Marcin Bogusz <bogusz.marcin@icloud.com>
2024-10-10 22:09:33 +0200

    Timer implementation.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ----------------------------- #
 PINK = "#e2979c"
@@ -9,13 +10,24 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ---------------------------- #
-
+def reset_timer():
+    canvas.itemconfig(timer_count, text="00:00")
 # ---------------------------- TIMER MECHANISM ------------------------ #
-
+def start_timer():
+    global reps
+    count_down(1 * 60)
 # ---------------------------- COUNTDOWN MECHANISM -------------------- #
-
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 0:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer_count, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro App")
@@ -25,14 +37,14 @@ timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW,
                     font=(FONT_NAME, 45, "bold"))
 check_label = Label(text="✓", fg=GREEN, bg=YELLOW,
                     font=(FONT_NAME, 35, "bold"))
-start_button = Button(text="Start", highlightthickness=0)
-reset_button = Button(text="Reset", highlightthickness=0)
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 
 canvas = Canvas(width=202, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white",
-                   font=(FONT_NAME, 35, "bold"))
+timer_count = canvas.create_text(100, 130, text="00:00", fill="white",
+                                 font=(FONT_NAME, 35, "bold"))
 
 timer_label.grid(row=0, column=1)
 canvas.grid(row=1, column=1)
